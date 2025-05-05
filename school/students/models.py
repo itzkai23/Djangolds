3f33fd9a8391e7dc2cfd96782bc954b2c586426d
itskai <imnotkairri@gmail.com>
2025-05-05 13:06:41 +0800

    zx

--- school/students/models.py
+++ school/students/models.py
@@ -3,14 +3,14 @@ from django.db import models
 class Student(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    address = models.TextField(default="Pampano St, Maya-Maya St, Malabon, Metro Manila")
+    address = models.CharField(max_length=50, default="Pampano St, Maya-Maya St, Malabon, Metro Manila")
     email = models.EmailField(unique=True)
     age = models.PositiveIntegerField()
     gender = models.CharField(
         max_length=10,
         choices=[('Male', 'Male'), ('Female', 'Female')]
     )
-    interest = models.TextField(max_length=50, default=" ")
+    interest = models.CharField(max_length=50, default=" ")
     course = models.TextField(max_length=50, blank=True)  # ✅ Allow course to be blank initially
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
